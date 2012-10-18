9eeb80e24cd15d572534252b40a90023fc21e9f3
Chris Abrams <mail@chrisabrams.com>
2012-10-18 00:00:32 -0700

    Added local storage for userID
    Signed-off-by: Chris Abrams <mail@chrisabrams.com>

--- public/js/app.js
+++ public/js/app.js
@@ -77,120 +77,134 @@
 
 	$(document).on('pageshow', '#search', function() {
 
-		$.ajax({
-			url: 'http://local.m.sharedcinema.com:3000/users',
-			type: "POST",
-			headers: {
-				"Accept": 'application/json'
-			},                                                          
-			error: function(res) {
-				console.log("user error")
-				console.log(res.responseText)
-			},
-			success: function(res) {
-				console.log("user response")
-				console.log(res)
+		fetchUser(function() {
 
-				var userID = res._id;
-				var timer = null;
-				var $searchInput = $('#search .ui-input-search [data-type=search]');
-
-				$searchInput.keyup(function(e) {
-					var _this = this;
-
-					clearTimeout(timer); 
-       				timer = setTimeout(function() {
-       					$.ajax({
-							url: 'http://local.m.sharedcinema.com:3000/search',
-							data: {
-								q: $(_this).val(),
-								user_id: userID
-							},
-							type: "GET",
-							headers: {
-								Accept: 'application/json'
-							},                                                             
-							success: successResponse,
-							error: function(msg) {
-								console.log(msg.responseText)
-							}
-						});
-       				}, 500)
-
-					function successResponse(res) {
-						console.log(res);
-						return;
-						var list = $('#search-list');
-						list.html('');
-
-						$.each(res.videos, function(index, video) {
-							//List element
-							var li = document.createElement('li');
-
-							//Anchor element
-							var a  = document.createElement('a');
-							a.href = '#';
-							//a.innerHTML = video.title;
-
-							//Left
-							var left = document.createElement('div');
-							left.className += ' video-list-left';
-
-							//Image
-							var img = document.createElement('img');
-							img.className += ' video-list-img';
-							img.src = video.thumb;
-							left.appendChild(img);
-
-							//Right
-							var right = document.createElement('div');
-							right.className += ' video-list-right';
-
-							//Title
-							var h3 = document.createElement('h3');
-							h3.innerHTML = video.title;
-							right.appendChild(h3);
-
-							//Description
-							var span = document.createElement('span');
-							span.innerHTML = video.desc;
-							right.appendChild(span);
-
-							//Append left and right to anchor
-							a.appendChild(left);
-							a.appendChild(right);
-
-							//Thumb
-							var thumb = document.createElement('button');
-							thumb.className += ' thumb';
-							thumb.type = 'button';
-							thumb.innerHTML = '&nbsp;';
-							a.appendChild(thumb);
-
-							//Append anchor to list
-							li.appendChild(a);
-
-							//Append list element to list
-							list.append(li);
-							//list.append('<li><a href="#">'+video.title+'</a></li>');
-						});
-
-						list.listview('refresh');
-
-						list.on('click', function(e) {
-							e.preventDefault();
-
-							if(e.target.tagName == "BUTTON") {
-								var el = e.target;
-
-								console.log("I am the upvote button");
-							}
-						});
-					}
-				});
-
-			}
 		});
 	});
+
+	function fetchUser() {
+		//Is userID in localstorage?
+		var userID = localStorage.getItem('sc-userID');
+
+		if(userID) {console.log(1)
+			userSuccessResponse(userID);
+		} else {console.log(2)
+			$.ajax({
+				url: 'http://local.m.sharedcinema.com:3000/users',
+				type: "POST",
+				headers: {
+					"Accept": 'application/json'
+				},                                                          
+				error: function(res) {
+					console.log("user error")
+					console.log(res.responseText)
+				},
+				success: function(res) {
+					var userID = res._id;
+					localStorage.setItem('sc-userID', userID);
+					userSuccessResponse(userID);
+				}
+			});
+		}
+	}
+
+	function successResponse(res) {
+
+		var list = $('#search-list');
+		list.html('');
+
+		$.each(res.videos, function(index, video) {
+			//List element
+			var li = document.createElement('li');
+
+			//Anchor element
+			var a  = document.createElement('a');
+			a.href = '#';
+			//a.innerHTML = video.title;
+
+			//Left
+			var left = document.createElement('div');
+			left.className += ' video-list-left';
+
+			//Image
+			var img = document.createElement('img');
+			img.className += ' video-list-img';
+			img.src = video.thumb;
+			left.appendChild(img);
+
+			//Right
+			var right = document.createElement('div');
+			right.className += ' video-list-right';
+
+			//Title
+			var h3 = document.createElement('h3');
+			h3.innerHTML = video.title;
+			right.appendChild(h3);
+
+			//Description
+			var span = document.createElement('span');
+			span.innerHTML = video.desc;
+			right.appendChild(span);
+
+			//Append left and right to anchor
+			a.appendChild(left);
+			a.appendChild(right);
+
+			//Thumb
+			var thumb = document.createElement('button');
+			thumb.className += ' thumb';
+			thumb.type = 'button';
+			thumb.innerHTML = '&nbsp;';
+			a.appendChild(thumb);
+
+			//Append anchor to list
+			li.appendChild(a);
+
+			//Append list element to list
+			list.append(li);
+			//list.append('<li><a href="#">'+video.title+'</a></li>');
+		});
+
+		list.listview('refresh');
+
+		list.on('click', function(e) {
+			e.preventDefault();
+
+			if(e.target.tagName == "BUTTON") {
+				var el = e.target;
+
+				console.log("I am the upvote button");
+			}
+		});
+	}
+
+	function userSuccessResponse(userID) {
+		
+		var timer = null;
+		var $searchInput = $('#search .ui-input-search [data-type=search]');
+
+		$searchInput.keyup(function(e) {
+			var _this = this;
+
+			clearTimeout(timer); 
+			timer = setTimeout(function() {
+				$.ajax({
+					url: 'http://local.m.sharedcinema.com:3000/search',
+					data: {
+						q: $(_this).val(),
+						user_id: userID
+					},
+					type: "GET",
+					headers: {
+						Accept: 'application/json'
+					},                                                             
+					success: successResponse,
+					error: function(msg) {
+						console.log(msg.responseText)
+					}
+				});
+			}, 500);
+		});
+	}
 
 }(jQuery, window));
